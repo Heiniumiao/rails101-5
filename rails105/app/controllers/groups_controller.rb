1db79f9ac89e79d92aef3489e87ff0dc443eedaf
heiniumiao <baiyanhong0321@gmail.com>
2017-11-04 23:00:24 +0300

    use simple_format helper to decorate

--- rails105/app/controllers/groups_controller.rb
+++ rails105/app/controllers/groups_controller.rb
@@ -1,12 +1,13 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
-  before_action :find_group_and_check_permission, only: [:edit, :updata, :destroy]
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy, :join, :quit]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
   def index
     @groups = Group.all
   end
 
   def show
     @group = Group.find(params[:id])
+    @posts = @group.posts.recent.paginate(:page => params[:page], :per_page => 5)
   end
 
   def edit
@@ -20,6 +21,7 @@ class GroupsController < ApplicationController
     @group = Group.new(group_params)
     @group.user = current_user
     if @group.save
+      current_user.join!(@group)
       redirect_to groups_path
     else
       render :new
@@ -38,6 +40,31 @@ class GroupsController < ApplicationController
 
     @group.destroy
     redirect_to groups_path, alert: "Group deleted"
+  end
+
+  def join
+    @group = Group.find(params[:id])
+    if !current_user.is_member_of?(@group)
+      current_user.join!(@group)
+      flash[:notice]="Success Added!"
+    else
+      flash[:warning]="U R already group member!"
+    end
+
+    redirect_to group_path(@group)
+  end
+
+  def quit
+    @group = Group.find(params[:id])
+
+    if current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+      flash[:alert] = "Quitted!"
+    else
+      flash[:warning]="U R not group member XD"
+    end
+
+    redirect_to group_path(@group)
   end
 
   private
