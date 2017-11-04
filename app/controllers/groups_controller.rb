77706aad282a00d9b32d01fa3f9822c5208ec018
heiniumiao <baiyanhong0321@gmail.com>
2017-11-04 23:33:33 +0300

    111

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,84 @@
+class GroupsController < ApplicationController
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy, :join, :quit]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
+  def index
+    @groups = Group.all
+  end
+
+  def show
+    @group = Group.find(params[:id])
+    @posts = @group.posts.recent.paginate(:page => params[:page], :per_page => 5)
+  end
+
+  def edit
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    @group.user = current_user
+    if @group.save
+      current_user.join!(@group)
+      redirect_to groups_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @group.update(group_params)
+      redirect_to groups_path, notice: "Update Success"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+
+    @group.destroy
+    redirect_to groups_path, alert: "Group deleted"
+  end
+
+  def join
+    @group = Group.find(params[:id])
+    if !current_user.is_member_of?(@group)
+      current_user.join!(@group)
+      flash[:notice]="Success Added!"
+    else
+      flash[:warning]="U R already group member!"
+    end
+
+    redirect_to group_path(@group)
+  end
+
+  def quit
+    @group = Group.find(params[:id])
+
+    if current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+      flash[:alert] = "Quitted!"
+    else
+      flash[:warning]="U R not group member XD"
+    end
+
+    redirect_to group_path(@group)
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:title, :description)
+  end
+
+  def find_group_and_check_permission
+    @group = Group.find(params[:id])
+
+    if current_user != @group.user
+      redirect_to root_path, alert: "U have no permission."
+    end
+  end
+
+end
